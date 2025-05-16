e9b8833f275fe2e3da3d7f5472e1b0dfc78d163b
Yongjun-Kim-717 <rladydwns717@gmail.com>
2025-05-16 16:37:24 +0900

    FirstCommit

---
+++ Assets/@Scripts/CameraMove.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Vector3 Offset = new Vector3(0, 4, -10);
+    public Transform Player;
+
+    void Update()
+    {
+
+    }
+}
