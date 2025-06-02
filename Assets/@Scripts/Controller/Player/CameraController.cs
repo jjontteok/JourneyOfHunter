4aa69716c08555c16b9eeed5d6352c354545c686
jjontteok <nayeong0121@gmail.com>
2025-06-02 13:51:06 +0900

    feature/nayeong

--- Assets/@Scripts/Controller/Player/CameraController.cs
+++ Assets/@Scripts/Controller/Player/CameraController.cs
@@ -4,8 +4,12 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform player;
 
-    Vector3 _offset = new Vector3(0f, 5f, -10f);
+    Vector3 _offset = new Vector3(0f, 10f, -20f);
 
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(15, 0, 0));
+    }
     void Update()
     {
         transform.position = player.position + _offset;
