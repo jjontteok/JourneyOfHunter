d1496f7fef329ee82fc9c435bcd34a29833d1d55
jjontteok <nayeong0121@gmail.com>
2025-06-24 10:05:47 +0900

    feature/nayeong13

--- Assets/@Scripts/Manager/TimeManager.cs
+++ Assets/@Scripts/Manager/TimeManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using extension;
 
 public class TimeManager : Singleton<TimeManager>, IEventSubscriber
 {
@@ -15,24 +16,14 @@ public class TimeManager : Singleton<TimeManager>, IEventSubscriber
     WaitForSeconds _standardTime = new WaitForSeconds(1f);
 
     private Dictionary<float, Define.TimeOfDayType> _colorChangeTransitions;
-    private Dictionary<float, Define.TimeOfDayType> _skyBoxChangeTransitions;
 
     float _monsterTime;
     public float _dayTime;
-    float _duration = 12;
     Define.TimeOfDayType _currentTime;
     bool _isDoubleSpeed;
     bool _isPlaying = true;
     bool _isSkyBoxChange = false;
     float _toKey = 1;
-
-    public float Duration
-    {
-        get 
-        { 
-            return _duration * _toKey; 
-        }
-    }
 
     public bool IsDoubleSpeed
     {
@@ -40,7 +31,7 @@ public class TimeManager : Singleton<TimeManager>, IEventSubscriber
         set
         {
             _isDoubleSpeed = value;
-            _toKey = _isDoubleSpeed ? 0.2f : 1;
+            _toKey = _isDoubleSpeed ? 0.5f : 1;
             _standardTime = new WaitForSeconds(_toKey);
             EnvironmentManager.Instance.ToKey = _toKey;
             OnTimeSpeedChanged?.Invoke(_currentTime);
@@ -67,14 +58,6 @@ public class TimeManager : Singleton<TimeManager>, IEventSubscriber
             { 22f, Define.TimeOfDayType.Evening },
             { 37f, Define.TimeOfDayType.Night },
             { 51f, Define.TimeOfDayType.Morning }
-        };
-        //값에 해당하는 스카이박스 변경될 때의 시각
-        _skyBoxChangeTransitions = new()
-        {
-            { GetColorChangeTime(Define.TimeOfDayType.Noon)+4f, Define.TimeOfDayType.Noon },
-            { GetColorChangeTime(Define.TimeOfDayType.Evening)+4f, Define.TimeOfDayType.Evening },
-            { GetColorChangeTime(Define.TimeOfDayType.Night)+5f, Define.TimeOfDayType.Night },
-            { GetColorChangeTime(Define.TimeOfDayType.Morning)+1f, Define.TimeOfDayType.Morning },
         };
     }
 
@@ -132,6 +115,7 @@ public class TimeManager : Singleton<TimeManager>, IEventSubscriber
     {
         StartCoroutine(DayTimer());
     }
+
     IEnumerator DayTimer()
     {
         while (true)
@@ -139,50 +123,20 @@ public class TimeManager : Singleton<TimeManager>, IEventSubscriber
             yield return _standardTime;
             _dayTime += 1;
             Debug.Log(_dayTime);
+            UIManager.Instance.TemporaryTimeText.text = $"시간 + {_dayTime}";
             
             if (_colorChangeTransitions.TryGetValue(_dayTime, out var newColorTimeOfDay))
             {
                 //현재 스카이박스 색 변경
                 OnColorChanged?.Invoke(newColorTimeOfDay);
-                _currentTime = GetNextType(newColorTimeOfDay);
+                _currentTime = Extension.GetNextType(newColorTimeOfDay);
             }
-            //newSkyBox를 다음 스카이박스로 변경
-            else if(_skyBoxChangeTransitions.TryGetValue(_dayTime, out var curSkyBox))
-            {
-                //그다음 스카이박스가 변경될 시각 - 현재 스카이박스가 끝날 시각
-                _duration = GetTime(GetNextType(curSkyBox)) - GetTime(curSkyBox);
-                if(curSkyBox == Define.TimeOfDayType.Morning)
-                {
-                    _duration = GetTime(GetNextType(curSkyBox));
-                }
-                OnSkyBoxChanged?.Invoke(GetNextType(curSkyBox));
-            }
+
             if (_dayTime >= 53f)
             {
                 _dayTime = 0f;
             }
         }
-    }
-
-    float GetColorChangeTime(Define.TimeOfDayType type)
-    {
-        return _colorChangeTransitions.FirstOrDefault(x => x.Value == type).Key;
-    }
-
-    float GetTime(Define.TimeOfDayType type)
-    {
-        return _skyBoxChangeTransitions.FirstOrDefault(x => x.Value == type).Key;
-    }
-    Define.TimeOfDayType GetNextType(Define.TimeOfDayType type)
-    {
-        return type switch
-        {
-            Define.TimeOfDayType.Noon => Define.TimeOfDayType.Evening,
-            Define.TimeOfDayType.Evening => Define.TimeOfDayType.Night,
-            Define.TimeOfDayType.Night => Define.TimeOfDayType.Morning,
-            Define.TimeOfDayType.Morning => Define.TimeOfDayType.Noon,
-            _ => 0
-        };
     }
 
     #endregion
--- Assets/@Scripts/Manager/UIManager/StageSceneUI/UIManager.cs
+++ Assets/@Scripts/Manager/UIManager/StageSceneUI/UIManager.cs
@@ -1,13 +1,35 @@
+using TMPro;
 using UnityEngine;
 
 public class UIManager : Singleton<UIManager>
 {
     private GameObject _uiGame;
+    UI_Game ui_game;
 
+    public TMP_Text TemporaryText
+    {
+        get { return ui_game.temporaryText; }
+        set { ui_game.temporaryText = value; }
+    }
+
+    public TMP_Text TemporaryTimeText
+    {
+        get { return ui_game.temporaryTimeText; }
+        set { ui_game.temporaryTimeText = value; }
+    }
+
+    public TMP_Text TemoraryColorChangeText
+    {
+        get { return ui_game.temporaryColorChangeText; }
+        set { ui_game.temporaryColorChangeText = value; }
+    }
     protected override void Initialize()
     {
         base.Initialize();
         _uiGame = Instantiate(ObjectManager.Instance.UIGame);
+
+        //빌드 시 로그 확인용 임시 텍스트
+        ui_game = _uiGame.GetComponent<UI_Game>();
     }
 
     #region Activate UI
